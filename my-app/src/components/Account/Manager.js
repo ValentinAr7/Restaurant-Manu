c5b82779bccb32e3a70e16779dcd0628586d6ce5
ValentinAr7 <valio990210@gmail.com>
2023-09-03 08:42:05 +0300

    [Menu] Reservations button

--- my-app/src/components/Account/Manager.js
+++ my-app/src/components/Account/Manager.js
@@ -1,57 +1,50 @@
+import React, { useState } from "react";
 import { Button } from "@mui/material";
-import {
-  PersonOutline,
-  ShoppingCart,
-  LocalOffer,
-  ExitToApp,
-} from "@mui/icons-material";
+import { LocalOffer, ExitToApp } from "@mui/icons-material";
 import styles from "./manager.module.css";
+import Reservations from "../AccountMenu/Reservations";
 
 const Manager = ({ onClose }) => {
+  const [reservation, setReservation] = useState(false);
+
   const handleExitButton = () => {
     localStorage.removeItem("token");
   };
 
+  const handleReservationClick = () => {
+    setReservation(true);
+  };
+
   return (
-    <div className={styles.container}>
-      <Button
-        className={styles.button}
-        variant="text"
-        fullWidth
-        startIcon={<PersonOutline className={styles.icon} fontSize="large" />}
-      >
-        Моят профил
-      </Button>
+    <div>
+      {!reservation ? (
+        <div className={styles.container}>
+          <Button
+            className={styles.button}
+            variant="text"
+            fullWidth
+            startIcon={<LocalOffer className={styles.icon} />}
+            onClick={handleReservationClick}
+          >
+            Резервация
+          </Button>
 
-      <Button
-        className={styles.button}
-        variant="text"
-        fullWidth
-        startIcon={<ShoppingCart className={styles.icon} />}
-      >
-        Кошница
-      </Button>
+          <Button
+            className={styles.button}
+            variant="text"
+            fullWidth
+            startIcon={<ExitToApp className={styles.icon} />}
+            onClick={() => {
+              handleExitButton();
+              onClose();
+            }}
+          >
+            Изход
+          </Button>
+        </div>
+      ) : null}
 
-      <Button
-        className={styles.button}
-        variant="text"
-        fullWidth
-        startIcon={<LocalOffer className={styles.icon} />}
-      >
-        Предложения
-      </Button>
-      <Button
-        className={styles.button}
-        variant="text"
-        fullWidth
-        startIcon={<ExitToApp className={styles.icon} />}
-        onClick={() => {
-          handleExitButton();
-          onClose();
-        }}
-      >
-        Изход
-      </Button>
+      {reservation && <Reservations />}
     </div>
   );
 };
